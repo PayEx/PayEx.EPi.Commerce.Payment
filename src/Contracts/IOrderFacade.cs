38c165169794b0679e850e430502ff3b8aad3ec0
Karoline Klever <karoline.klever@epinova.no>
2014-06-03 08:23:20 +0200

    Added project to repository

---
+++ src/Contracts/IOrderFacade.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Epinova.PayExProvider.Contracts
+{
+    public interface IOrderFacade
+    {
+        string Initialize(long accountNumber, string purchaseOperation, long price, string priceArgList, string currency,
+            int vat, string orderId, string productNumber, string description,
+            string clientIpAddress, string clientIdentifier, string additionalValues, string externalId,
+            string returnUrl, string view, string agreementRef, string cancelUrl, string clientLanguage, string hash);
+
+        string Complete(long accountNumber, string orderRef, string hash);
+
+        string Capture(long accountNumber, int transactionNumber, long amount, string orderId, int vatAmount,
+            string additionalValues, string hash);
+    }
+}
---
+++ src/Facades/Order.cs
@@ -0,0 +1,33 @@
+﻿
+using Epinova.PayExProvider.Contracts;
+
+namespace Epinova.PayExProvider.Facades
+{
+    public class Order : IOrderFacade
+    {
+        private readonly PxOrder.PxOrderSoapClient _client;
+
+        public Order()
+        {
+            _client = new PxOrder.PxOrderSoapClient();
+        }
+
+        public string Initialize(long accountNumber, string purchaseOperation, long price, string priceArgList, string currency, int vat, string orderId, string productNumber, string description,
+            string clientIpAddress, string clientIdentifier, string additionalValues, string externalId, string returnUrl, string view, string agreementRef, string cancelUrl, string clientLanguage, string hash)
+        {
+            return _client.Initialize8(accountNumber, purchaseOperation, price, priceArgList, currency, vat, orderId,
+                                      productNumber, description, clientIpAddress, clientIdentifier, additionalValues,
+                                      externalId, returnUrl, view, agreementRef, cancelUrl, clientLanguage, hash);
+        }
+
+        public string Complete(long accountNumber, string orderRef, string hash)
+        {
+            return _client.Complete(accountNumber, orderRef, hash);
+        }
+
+        public string Capture(long accountNumber, int transactionNumber, long amount, string orderId, int vatAmount, string additionalValues, string hash)
+        {
+            return _client.Capture5(accountNumber, transactionNumber, amount, orderId, vatAmount, additionalValues, hash);
+        }
+    }
+}
