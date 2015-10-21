b76a8f6b1630ec5431c335498aadaea0fb3ebff7
Cyberlane <dogbertuk2000@gmail.com>
2015-10-21 17:45:58 +0200

    String interpolation is easier to read

--- src/PayEx.EPi.Commerce.Payment/Dectorators/PaymentInitializers/GenerateOrderNumber.cs
+++ src/PayEx.EPi.Commerce.Payment/Dectorators/PaymentInitializers/GenerateOrderNumber.cs
@@ -20,24 +20,24 @@ namespace PayEx.EPi.Commerce.Payment.Dectorators.PaymentInitializers
 
         public PaymentInitializeResult Initialize(PaymentMethod currentPayment, string orderNumber, string returnUrl, string orderRef)
         {
-            Log.InfoFormat("Generating order number for payment with ID:{0} belonging to order with ID: {1}", currentPayment.Payment.Id, currentPayment.OrderGroupId);
+            Log.Info($"Generating order number for payment with ID:{currentPayment.Payment.Id} belonging to order with ID: {currentPayment.OrderGroupId}");
             if (string.IsNullOrWhiteSpace(orderNumber))
                 orderNumber = _orderNumberGenerator.Generate(currentPayment.Cart);
 
-            Log.InfoFormat("Generated order number:{0} for payment with ID:{1} belonging to order with ID: {2}", orderNumber, currentPayment.Payment.Id, currentPayment.OrderGroupId);
+            Log.Info($"Generated order number:{orderNumber} for payment with ID:{currentPayment.Payment.Id} belonging to order with ID: {currentPayment.OrderGroupId}");
             currentPayment.Payment.OrderNumber = orderNumber;
 
             if (!string.IsNullOrWhiteSpace(currentPayment.Payment.Description))
             {
                 if (currentPayment.Payment.Description.Contains("{0}"))
                 {
-                    Log.InfoFormat("Including the ordernumber in the payment description for payment with ID:{0} belonging to order with ID: {1}", currentPayment.Payment.Id, currentPayment.OrderGroupId);
+                    Log.Info($"Including the ordernumber in the payment description for payment with ID:{currentPayment.Payment.Id} belonging to order with ID: {currentPayment.OrderGroupId}");
                     currentPayment.Payment.Description = string.Format(currentPayment.Payment.Description, orderNumber);
-                    Log.InfoFormat("Payment description is set to:{0} for payment with ID:{1} belonging to order with ID: {2}", currentPayment.Payment.Description, currentPayment.Payment.Id, currentPayment.OrderGroupId);
+                    Log.Info($"Payment description is set to:{currentPayment.Payment.Description} for payment with ID:{currentPayment.Payment.Id} belonging to order with ID: {currentPayment.OrderGroupId}");
                 }
             }
 
-            Log.InfoFormat("Finished generating order number for payment with ID:{0} belonging to order with ID: {1}", currentPayment.Payment.Id, currentPayment.OrderGroupId);
+            Log.Info($"Finished generating order number for payment with ID:{currentPayment.Payment.Id} belonging to order with ID: {currentPayment.OrderGroupId}");
             return _initializer.Initialize(currentPayment, orderNumber, returnUrl, orderRef);
         }
     }
