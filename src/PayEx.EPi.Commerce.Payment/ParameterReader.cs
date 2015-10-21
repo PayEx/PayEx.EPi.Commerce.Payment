d9e76818b2b0a100cd585ba8b51eff1901439991
Cyberlane <dogbertuk2000@gmail.com>
2015-10-21 17:13:35 +0200

    Code cleanup for consistency

--- src/PayEx.EPi.Commerce.Payment/ParameterReader.cs
+++ src/PayEx.EPi.Commerce.Payment/ParameterReader.cs
@@ -21,7 +21,7 @@ namespace PayEx.EPi.Commerce.Payment
 
         public static PaymentMethodDto.PaymentMethodParameterRow GetParameterByName(PaymentMethodDto paymentMethodDto, string name)
         {
-            PaymentMethodDto.PaymentMethodParameterRow[] rowArray = (PaymentMethodDto.PaymentMethodParameterRow[])paymentMethodDto.PaymentMethodParameter.Select(string.Format("Parameter = '{0}'", name));
+            var rowArray = (PaymentMethodDto.PaymentMethodParameterRow[])paymentMethodDto.PaymentMethodParameter.Select(string.Format("Parameter = '{0}'", name));
             if (rowArray.Length > 0)
                 return rowArray[0];
             throw new ArgumentNullException("Parameter named " + name + " for PayEx payment cannot be null");
