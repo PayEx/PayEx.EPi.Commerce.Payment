418e5dfae2138161e6fcce614301b64db4ff9a34
Cyberlane <dogbertuk2000@gmail.com>
2015-10-21 18:29:51 +0200

    Expressions make nice properties

--- src/PayEx.EPi.Commerce.Payment/Models/Result/TransactionResult.cs
+++ src/PayEx.EPi.Commerce.Payment/Models/Result/TransactionResult.cs
@@ -1,6 +1,5 @@
 ﻿using System.Xml.Serialization;
 using Newtonsoft.Json;
-using PayEx.EPi.Commerce.Payment.Contracts;
 
 namespace PayEx.EPi.Commerce.Payment.Models.Result
 {
@@ -16,45 +15,15 @@ namespace PayEx.EPi.Commerce.Payment.Models.Result
         [XmlElement("invoice")]
         public InvoiceDetails Invoice { get; set; }
 
-        public string CustomerName
-        {
-            get
-            {
-                return AddressCollection.MainAddress != null ? AddressCollection.MainAddress.CustomerName : string.Empty;
-            }
-        }
+        public string CustomerName => AddressCollection.MainAddress != null ? AddressCollection.MainAddress.CustomerName : string.Empty;
 
-        public string Address
-        {
-            get
-            {
-                return AddressCollection.MainAddress != null ? AddressCollection.MainAddress.Address : string.Empty;
-            }
-        }
+        public string Address => AddressCollection.MainAddress != null ? AddressCollection.MainAddress.Address : string.Empty;
 
-        public string PostNumber
-        {
-            get
-            {
-                return AddressCollection.MainAddress != null ? AddressCollection.MainAddress.PostNumber : string.Empty;
-            }
-        }
+        public string PostNumber => AddressCollection.MainAddress != null ? AddressCollection.MainAddress.PostNumber : string.Empty;
 
-        public string City
-        {
-            get
-            {
-                return AddressCollection.MainAddress != null ? AddressCollection.MainAddress.City : string.Empty;
-            }
-        }
+        public string City => AddressCollection.MainAddress != null ? AddressCollection.MainAddress.City : string.Empty;
 
-        public string Country
-        {
-            get
-            {
-                return AddressCollection.MainAddress != null ? AddressCollection.MainAddress.Country : string.Empty;
-            }
-        }
+        public string Country => AddressCollection.MainAddress != null ? AddressCollection.MainAddress.Country : string.Empty;
 
         public override string ToString()
         {
