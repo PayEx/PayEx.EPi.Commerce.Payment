0f65d4ef989647e56a60d939b1e109e58b508b15
Cyberlane <dogbertuk2000@gmail.com>
2015-10-21 17:12:02 +0200

    Array cannot be null

--- src/PayEx.EPi.Commerce.Payment/MasterPassConfigurePayment.ascx.cs
+++ src/PayEx.EPi.Commerce.Payment/MasterPassConfigurePayment.ascx.cs
@@ -129,7 +129,7 @@ namespace PayEx.EPi.Commerce.Payment
         private PaymentMethodDto.PaymentMethodParameterRow GetParameterByName(string name)
         {
             PaymentMethodDto.PaymentMethodParameterRow[] rowArray = (PaymentMethodDto.PaymentMethodParameterRow[])_paymentMethodDto.PaymentMethodParameter.Select(string.Format("Parameter = '{0}'", name));
-            if ((rowArray != null) && (rowArray.Length > 0))
+            if ((rowArray.Length > 0))
             {
                 return rowArray[0];
             }
